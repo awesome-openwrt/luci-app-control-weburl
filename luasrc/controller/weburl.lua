30c193521beac5813c6ee8c605472ca2f5e1db2e
stuarthua <stuarthua.cn@gmail.com>
2020-06-10 20:10:05 +0800

    repost by shell.

--- luasrc/controller/weburl.lua
+++ luasrc/controller/weburl.lua
@@ -3,9 +3,10 @@ module("luci.controller.weburl", package.seeall)
 function index()
     if not nixio.fs.access("/etc/config/weburl") then return end
 
-    entry({"admin", "custom"}, firstchild(), "我的", 89).dependent = false
-    entry({"admin", "custom", "weburl"}, cbi("weburl"), _("网址过滤"), 603).dependent = true
-    entry({"admin", "custom", "weburl", "status"}, call("status")).leaf = true
+    entry({"admin", "control"}, firstchild(), "Control", 44).dependent = false
+    entry({"admin", "control", "weburl"}, cbi("weburl"), _("网址过滤"), 12).dependent =
+        true
+    entry({"admin", "control", "weburl", "status"}, call("status")).leaf = true
 end
 
 function status()
--- luasrc/model/cbi/weburl.lua
+++ luasrc/model/cbi/weburl.lua
@@ -1,19 +1,22 @@
 local o = require "luci.sys"
 local a, t, e
-a = Map("weburl", translate("网址过滤"), translate("在这里设置关键词过滤，可以是URL里任意字符，可以过滤如视频网站、QQ、迅雷、淘宝。。。"))
+a = Map("weburl", translate("网址过滤"), translate(
+            "在这里设置关键词过滤，可以是URL里任意字符，可以过滤如视频网站、QQ、迅雷、淘宝。。。"))
 a.template = "weburl/index"
-t = a:section(TypedSection, "basic", translate("运行状态"))
+t = a:section(TypedSection, "basic", translate("Running Status"))
 t.anonymous = true
 e = t:option(DummyValue, "weburl_status", translate("当前状态"))
 e.template = "weburl/weburl"
 e.value = translate("Collecting data...")
-t = a:section(TypedSection, "basic", translate("基本设置"), translate("一般来说普通过滤效果就很好了，强制过滤会使用更复杂的算法导致更高的CPU占用。"))
+t = a:section(TypedSection, "basic", translate("基本设置"), translate(
+                  "一般来说普通过滤效果就很好了，强制过滤会使用更复杂的算法导致更高的CPU占用。"))
 t.anonymous = true
 e = t:option(Flag, "enable", translate("开启"))
 e.rmempty = false
 e = t:option(Flag, "algos", translate("强效过滤"))
 e.rmempty = false
-t = a:section(TypedSection, "macbind", translate("关键词设置"), translate("黑名单MAC不设置为全客户端过滤，如设置只过滤指定的客户端。过滤时间可不设置。"))
+t = a:section(TypedSection, "macbind", translate("关键词设置"), translate(
+                  "黑名单MAC不设置为全客户端过滤，如设置只过滤指定的客户端。过滤时间可不设置。"))
 t.template = "cbi/tblsection"
 t.anonymous = true
 t.addremove = true
